3032ced79652026e22369a58e70aaa7a86e820b6
Elliott Bristow <admin@ellitopia.com>
2024-08-19 11:00:24 +0200

    Glo Checklists port to 17.0 for App Store (#1)
    * Initial commit to 17.0
    * pre-commit fixes

---
+++ glo_checklists/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Checklist Anything",
+    "summary": """
+        Add checklists to any model via a template.""",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "category": "Technical",
+    "version": "17.0.1.0.0",
+    "depends": ["base", "mail"],
+    "data": [
+        "security/groups.xml",
+        "security/ir.model.access.csv",
+        "views/checklist.xml",
+        "views/settings.xml",
+        "wizards/help_popup.xml",
+    ],
+    "demo": [],
+    "license": "LGPL-3",
+    "price": 49,
+    "currency": "GBP",
+    "application": True,
+}
