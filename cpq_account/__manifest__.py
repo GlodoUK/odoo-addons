bd50dbbd0c2c1a161cf2cbddcfd56b6dad5cdb17
Ahmed Hagi <ahmedosman@mac.com>
2024-11-14 11:38:35 +0000

    GH164696 - cpq v17

---
+++ cpq_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "cpq_account",
+    "summary": "Glue module between CPQ and account",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/cpq",
+    "category": "Uncategorized",
+    "version": "17.0.1.0.0",
+    "depends": ["account", "cpq"],
+    "auto_install": True,
+    "license": "LGPL-3",
+}
