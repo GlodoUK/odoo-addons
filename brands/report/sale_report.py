d2788fd360abc075f7eb0bc2d8fb47b82b9292c8
Felix Otoo <ofelix03@gmail.com>
2024-11-19 15:01:59 +0000

    fix bug when opening a product form
    issue occurs after installing the brands module

--- brands/report/sale_report.py
+++ brands/report/sale_report.py
@@ -7,16 +7,13 @@ class SaleReport(models.Model):
     brand_id = fields.Many2one("glo.brand", "Sale Brand", readonly=True)
     product_tmpl_brand_id = fields.Many2one("glo.brand", "Product Brand", readonly=True)
 
-    def _query(self, with_clause="", fields=False, groupby="", from_clause=""):
-        if not fields:
-            fields = {}
-        if not groupby:
-            groupby = ""
-        fields.update(
-            {
-                "brand_id": ", s.brand_id as brand_id",
-                "product_tmpl_brand_id": ", t.brand_id as product_tmpl_brand_id",
-            }
-        )
+    def _select_additional_fields(self):
+        res = super()._select_additional_fields()
+        res["brand_id"] = "s.brand_id"
+        res["product_tmpl_brand_id"] = "t.brand_id"
+        return res
+
+    def _group_by_sale(self):
+        groupby = super()._group_by_sale()
         groupby += ", s.brand_id, t.brand_id"
-        return super()._query(with_clause, fields, groupby, from_clause)
+        return groupby
