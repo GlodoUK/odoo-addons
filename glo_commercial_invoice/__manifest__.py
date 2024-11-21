7796a9fe3be08c822b889ead8b8938c389be4c35
Kobby Folson <kobbyfolson@gmail.com>
2024-11-21 18:37:26 +0000

    add: new module glo_commercial_invoice

---
+++ glo_commercial_invoice/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "glo_commercial_invoice",
+    "summary": "Glo Commercial Invoice",
+    "version": "17.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "author": "Glo Networks",
+    "license": "Other proprietary",
+    "depends": ["sale", "stock", "sale_stock"],
+    "data": [
+        "views/stock_picking_views.xml",
+        "report/glo_commercial_invoice_views.xml",
+        "report/stock_report_views.xml",
+    ],
+}
