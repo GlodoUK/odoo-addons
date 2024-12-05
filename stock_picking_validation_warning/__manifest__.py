c277482bcae773458fd6914cb94ba2bc2baa933c
Ahmed Hagi <ahmedosman@mac.com>
2024-12-05 15:47:46 +0000

    pre-commit fight p2, can't win

--- stock_picking_validation_warning/__manifest__.py
+++ stock_picking_validation_warning/__manifest__.py
@@ -6,6 +6,6 @@
     "category": "Uncategorized",
     "version": "13.0.1.0.0",
     "depends": ["stock"],
-    "data": ["wizards/stock_picking_warning.xml",],
+    "data": ["wizards/stock_picking_warning.xml"],
     "license": "AGPL-3",
 }
