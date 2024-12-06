ef8387ca2dc4b9f64a80ef02c1d324948bcdb268
GarethGloSystems <gareth@glo.systems>
2024-12-06 12:20:58 +0000

    [FEAT] report_layout_no_header_no_footer module

--- report_layout_no_header_no_footer/__manifest__.py
+++ report_layout_no_header_no_footer/__manifest__.py
@@ -2,8 +2,8 @@
     "name": "Report Layout No Header No Footer",
     "version": "17.0.1.0.0",
     "author": "Glo Networks",
-    "website": "https://glo.systems",
+    "website": "https://github.com/GlodoUK/odoo-addons",
     "depends": ["web"],
-    "data": ["views/report_layout"],
+    "data": ["views/report_layout.xml"],
     "license": "LGPL-3",
 }
