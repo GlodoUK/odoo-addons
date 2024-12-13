83c0a1d1cf1ea50a5fada065b4506c90e3f16b32
GarethGloSystems <gareth@glo.systems>
2024-12-13 12:30:56 +0000

    Unit tests

--- purchase_stock_update_move_date/tests/test_update_move_date.py
+++ purchase_stock_update_move_date/tests/test_update_move_date.py
@@ -26,7 +26,40 @@ class TestUpdateMoveDate(TransactionCase):
         )
 
     @freeze_time("2025-01-01 06:00:00")
-    def test_update_move_date(self):
+    def test_update_move_date_order(self):
+        order_id = self.env["purchase.order"].create(
+            {
+                "partner_id": self.partner_id.id,
+            }
+        )
+
+        self.env["purchase.order.line"].create(
+            {
+                "order_id": order_id.id,
+                "product_id": self.product_id.id,
+            }
+        )
+
+        order_id.button_confirm()
+
+        self.assertEqual(
+            order_id.picking_ids.move_ids.date,
+            datetime(2025, 1, 1, 6, 0, 0),  # 6AM 2025 1st Jan
+        )
+
+        date_planned_initial = order_id.date_planned
+
+        order_id.write(
+            {"date_planned": date_planned_initial + relativedelta(days=5)}
+        )
+
+        self.assertEqual(
+            order_id.picking_ids.move_ids.date,
+            datetime(2025, 1, 6, 6, 0, 0),  # 6AM 2025 6th Jan
+        )
+
+    @freeze_time("2025-01-01 06:00:00")
+    def test_update_move_date_order_line(self):
         order_id = self.env["purchase.order"].create(
             {
                 "partner_id": self.partner_id.id,
