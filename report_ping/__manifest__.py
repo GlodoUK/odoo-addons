ce0c3cb27bf1e47ddd6dd8c0f66096022b99bb84
Ahmed Hagi <ahmedosman@mac.com>
2025-01-08 10:53:06 +0000

    GH164696 - Mail v12

---
+++ report_ping/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "report_ping",
+    "summary": "report pings",
+    "author": "Glodo",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "license": "Other proprietary",
+    "category": "Reporting",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "base",
+        "web",
+        "mail",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/mail_template.xml",
+        "views/views.xml",
+    ],
+    "demo": [],
+}
