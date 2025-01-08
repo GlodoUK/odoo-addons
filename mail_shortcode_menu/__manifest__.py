537fceedc86ad1a2db1100028f49eaa5ef86190c
Ahmed Hagi <ahmedosman@mac.com>
2025-01-08 10:45:25 +0000

    GH164696 - Mail v13

---
+++ mail_shortcode_menu/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "mail_shortcode_menu",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "author": "Glodo",
+    "summary": """Adds a menu to edit canned responses without livechat module
+being installed""",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["mail", "base"],
+    "data": ["views/mail_shortcode.xml"],
+    "installable": True,
+    "application": False,
+}
