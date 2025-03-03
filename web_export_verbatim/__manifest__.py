f2dde61e5c4c679ef03ec14e2427528d9f10db9c
Karl Southern <karl@theangryangel.co.uk>
2025-03-03 13:45:07 +0000

    feat(web): migration from GlodoUK/web

---
+++ web_export_verbatim/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "web_export_verbatim",
+    "version": "12.0.1.0.0",
+    "author": "Glo Networks",
+    "license": "Other proprietary",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "category": "Tools",
+    "data": ["views/templates.xml"],
+    "depends": [
+        "base",
+        "web",
+        "web_export_view",
+    ],
+}
