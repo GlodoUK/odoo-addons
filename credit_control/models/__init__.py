f723ca30ca5fe033fb1d858abd49ebacaebea3ef
Karl Southern <karl@theangryangel.co.uk>
2025-03-03 13:52:54 +0000

    feat(account): migration from glodouk/account

---
+++ credit_control/models/__init__.py
@@ -0,0 +1,6 @@
+from . import credit_control_classification
+from . import credit_control_policy
+from . import credit_control_rule
+from . import mail_activity
+from . import res_partner
+from . import sale
