b2eaa9654b8d28852b91311dcc25174a9da6c8f4
Karl <karl@theangryangel.co.uk>
2025-03-12 22:36:26 +0000

    Update __manifest__.py

--- res_partner_search_create/__manifest__.py
+++ res_partner_search_create/__manifest__.py
@@ -8,6 +8,6 @@ Partner utility functions to search or create from values
     "website": "https://github.com/GlodoUK/odoo-addons",
     "depends": ["base_iso3166"],
     "data": [],
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "external_dependencies": {"bin": [], "python": []},
 }
