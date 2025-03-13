3a1be8b770d68f973876169e80b07db2515e23c2
GarethGloSystems <gareth@glo.systems>
2025-03-13 16:55:24 +0000

    Add helpdesk_ticket_category to odoo-addons instead of glo-odoo

--- helpdesk_portal_new_ticket/__manifest__.py
+++ helpdesk_portal_new_ticket/__manifest__.py
@@ -3,11 +3,11 @@
     "version": "18.0.1.0.0",
     "author": "Glo Networks",
     "website": "https://github.com/GlodoUK/odoo-addons",
-    "depends": ["helpdesk_ticket_type"],
+    "depends": ["helpdesk_ticket_category"],
     "data": [
         "views/portal_breadcrumbs.xml",
         "views/portal_searchbar.xml",
         "views/template_helpdesk_ticket_new.xml",
     ],
-    "license": "LGPL-3",
+    "license": "Other proprietary",
 }
