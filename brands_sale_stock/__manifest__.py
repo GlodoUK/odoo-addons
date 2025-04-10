1cc78fe6adc049760f7fad05ea67dec1e0b70acc
GarethGloSystems <gareth@glo.systems>
2025-04-10 09:46:30 +0100

    [ci skip] Upgrade brands_sale_stock to 18.0

---
+++ brands_sale_stock/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Brands Sale Stock",
+    "version": "18.0.1.0.0",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "depends": ["brands", "sale_stock"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "license": "Other proprietary",
+}
---
+++ brands_sale_stock/models/stock_picking.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    brand_id = fields.Many2one(
+        related="group_id.sale_id.brand_id",
+        store=True,
+    )
