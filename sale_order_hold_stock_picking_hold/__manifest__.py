9f7c1b2f5cfe0ee13f9d65e2ac8bf88ebd1f20f4
GarethGloSystems <gareth@glo.systems>
2025-04-25 10:46:45 +0100

    Upgrade sale_order_hold_stock_picking_hold to 18.0

---
+++ sale_order_hold_stock_picking_hold/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Sale Order Hold Stock Picking Hold",
+    "summary": "Integrate sale_order_hold with stock_picking_hold",
+    "version": "18.0.1.0.0",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "auto_install": True,
+    "depends": [
+        "stock_picking_hold",
+        "sale_order_hold",
+    ],
+    "license": "LGPL-3",
+}
