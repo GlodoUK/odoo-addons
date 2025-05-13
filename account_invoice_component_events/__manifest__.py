bae963e5fe4e00b6beba9b28103c8763f3947a35
GarethGloSystems <120731454+GarethGloSystems@users.noreply.github.com>
2025-05-13 09:46:05 +0100

    [T14406] Upgrade account_invoice_component_events (#92)
    * [T14406] Upgrade account_invoice_component_events
    
    * Add test for invoice payment
    
    * Fix requirements.txt
    
    Stupid pre-commit
    
    ---------
    
    Co-authored-by: ellbristow <elliott@glo.systems>
    Co-authored-by: Elliott Bristow <admin@ellitopia.com>

---
+++ account_invoice_component_events/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Account Invoice Component Events",
+    "version": "18.0.1.0.0",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "depends": ["account", "component_event", "connector"],
+    "external_dependencies": {"python": ["mock"]},
+    "license": "Other proprietary",
+}
