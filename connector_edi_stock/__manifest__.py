c48096d1c860ab0f7b40c58fc7960ee33a71c32e
ellbristow <elliott@glo.systems>
2025-05-14 13:27:32 +0200

    Add missing dependency

--- connector_edi_stock/__manifest__.py
+++ connector_edi_stock/__manifest__.py
@@ -3,7 +3,11 @@
     "version": "18.0.1.0.0",
     "author": "Glo Networks",
     "website": "https://github.com/GlodoUK/odoo-addons",
-    "depends": ["connector_edi", "stock_picking_component_events"],
+    "depends": [
+        "connector_edi",
+        "stock_picking_component_events",
+        "connector_edi_sale",
+    ],
     "data": ["data/edi_route_event_data.xml"],
     "license": "Other proprietary",
 }
