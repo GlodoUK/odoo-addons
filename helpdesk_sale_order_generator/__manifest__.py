f4c5bdf16e68944fee075d393daef05600ebd795
ellbristow <elliott@glo.systems>
2025-05-15 15:47:30 +0200

    Bump version Add new logo remove commented out code

--- helpdesk_sale_order_generator/__manifest__.py
+++ helpdesk_sale_order_generator/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Helpdesk Sale Order Generator",
-    "version": "15.0.1.0.1",
+    "version": "18.0.1.0.1",
     "category": "Services/Helpdesk",
     "author": "Glo Networks",
     "website": "https://github.com/GlodoUK/odoo-addons",
