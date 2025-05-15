577d1fd3b9832907cf186a6acd82d5cf5e96daed
Karl Southern <karl@theangryangel.co.uk>
2025-05-15 10:09:25 +0100

    fix(connector_edi,connector_edi_sale,connector_edi_stock): support connector_no_export

--- connector_edi/components/envelope_listener.py
+++ connector_edi/components/envelope_listener.py
@@ -6,7 +6,17 @@ class EdiEnvelopeListener(Component):
     _inherit = "base.event.listener"
     _apply_on = ["edi.envelope"]
 
+    def no_connector_export(self, record):
+        # FIXME: duplicated because we've inherited off base.event.listener rather than
+        # base.connector.listener.
+        return record.env.context.get("no_connector_export") or record.env.context.get(
+            "connector_no_export"
+        )
+
     def on_pending(self, record):
+        if self.no_connector_export(record):
+            return
+
         if record.direction == "in":
             opts = {}
             if record.route_id:
