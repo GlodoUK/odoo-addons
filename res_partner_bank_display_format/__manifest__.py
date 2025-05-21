1806d49569029c5c1f09970093a1e5b22e5c6aca
Karl Southern <karl@theangryangel.co.uk>
2025-05-21 11:18:47 +0100

    feat(res_partner_bank_display_format): initial implementation

---
+++ res_partner_bank_display_format/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "res_partner_bank_display_format",
+    "summary": "Customise the format of the partner bank",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "category": "Uncategorized",
+    "version": "18.0.1.0.0",
+    "depends": ["account"],
+    "data": [
+        "views/res_partner_bank.xml",
+    ],
+    "license": "LGPL-3",
+}
