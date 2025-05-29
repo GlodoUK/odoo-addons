3dd3ed52c1a4f538f76829ffb7cd46b31107d4aa
GarethGloSystems <gareth@glo.systems>
2025-05-29 11:47:18 +0100

    Initial _compute_tax_id logic

--- product_variant_specific_tax_purchase/models/purchase_order_line.py
+++ product_variant_specific_tax_purchase/models/purchase_order_line.py
@@ -5,4 +5,23 @@ class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
     def _compute_tax_id(self):
-        pass
+        has_variant_supplier_taxes_id = self.filtered(
+            lambda line: line.variant_supplier_taxes_id
+        )
+
+        for line in has_variant_supplier_taxes_id:
+            line = line.with_company(line.company_id)
+            fpos = (
+                line.order_id.fiscal_position_id
+                or line.order_id.fiscal_position_id._get_fiscal_position(
+                    line.order_id.partner_id
+                )
+            )
+            taxes = line.product_id.variant_supplier_taxes_id._filter_taxes_by_company(
+                line.company_id
+            )
+            line.taxes_id = fpos.map_tax(taxes)
+
+        return super(
+            PurchaseOrderLine, self - has_variant_supplier_taxes_id
+        )._compute_purchase_price()
