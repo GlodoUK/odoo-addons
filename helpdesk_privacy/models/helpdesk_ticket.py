57b4124ae9d444b7ab1574774f56b0ff708a87ae
Karl Southern <karl@theangryangel.co.uk>
2025-06-06 11:43:01 +0100

    fix(helpdesk_privacy): ensure that we do not subscribe users who do not already follow the ticket when processing a message_update. fix(brands): fix due to upstream changes under https://github.com/odoo/odoo/commit/09d5f83ee88c1f786a678d0aea6de85c2391505c
    This is not a perfect implementation, but I'm unclear how to better handle better without a lot of "what-ifs":
    
    i.e. In the scenario User A who is following a private ticket CC's User B who is not, Odoo needs to accept the email, but we cannot just hard block the email as it should be posted. But we also can't just blackhole it.
    
    This then needs to handle BCC, etc.
    
    What I've chosen to do is accept the email, post it, but ensure that only the existing users are subscribed.
    
    This has the side effect of allowing parties without access the ability to potentially post message, but not be auto-subscribed.
    
    Fixes GH181524

--- helpdesk_privacy/models/helpdesk_ticket.py
+++ helpdesk_privacy/models/helpdesk_ticket.py
@@ -25,3 +25,43 @@ class HelpdeskTicket(models.Model):
             ("id", "child_of", self.commercial_partner_id.id),
             ("id", "not in", self.message_partner_ids.ids),
         ]
+
+    def message_update(self, msg, update_vals=None):
+        return super(
+            HelpdeskTicket,
+            self.with_context(
+                helpdesk_privacy_only_subscribe_existing_partners=self.is_private
+            ),
+        ).message_update(msg, update_vals=update_vals)
+
+    def _message_subscribe(self, partner_ids=None, subtype_ids=None, customer_ids=None):
+        """
+        Override to enforce privacy and prevent users from subscribing to tickets
+        that they should not have access to.
+
+        This can be achieved by unauthorised user responding to an email with Odoo
+        headers on it. i.e. it is forwarded to them by someone who does have access.
+
+        This is not a perfect implementation, but I'm unclear how to better handle
+        better without a lot of "what-ifs":
+
+        i.e. In the scenario User A who is following a private ticket CC's User B who is
+        not, Odoo needs to accept the email, but we cannot just hard block the email as
+        it should be posted. But we also can't just blackhole it.
+
+        This then needs to handle BCC, etc.
+
+        What I've chosen to do is accept the email, post it, but ensure that only the
+        existing users are subscribed.
+
+        This has the side effect of allowing parties without access the ability to
+        potentially post message, but not be auto-subscribed.
+        """
+        if partner_ids and self._context.get(
+            "helpdesk_privacy_only_subscribe_existing_partners"
+        ):
+            partner_ids = [i for i in partner_ids if i in self.message_partner_ids.ids]
+
+        return super()._message_subscribe(
+            partner_ids=partner_ids, subtype_ids=subtype_ids, customer_ids=customer_ids
+        )
--- helpdesk_privacy/tests/__init__.py
+++ helpdesk_privacy/tests/__init__.py
@@ -1,1 +1,3 @@
+from . import common
 from . import test_helpdesk_team_privacy_visibility
+from . import test_message_subscribe
