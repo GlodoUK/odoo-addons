c3aefcfee9e8ab5321c056f97ea4dcb8ff88134d
Karl Southern <karl@theangryangel.co.uk>
2025-06-06 11:43:01 +0100

    fix(helpdesk_privacy): ensure that we do not subscribe users who do not already follow the ticket when processing a message_update. fix(brands): fix due to upstream changes under https://github.com/odoo/odoo/commit/09d5f83ee88c1f786a678d0aea6de85c2391505c
    This is not a perfect implementation, but I'm unclear how to better handle better without a lot of "what-ifs":
    
    i.e. In the scenario User A who is following a private ticket CC's User B who is not, Odoo needs to accept the email, but we cannot just hard block the email as it should be posted. But we also can't just blackhole it.
    
    This then needs to handle BCC, etc.
    
    What I've chosen to do is accept the email, post it, but ensure that only the existing users are subscribed.
    
    This has the side effect of allowing parties without access the ability to potentially post message, but not be auto-subscribed.
    
    Fixes GH181524

--- helpdesk_privacy/tests/test_helpdesk_team_privacy_visibility.py
+++ helpdesk_privacy/tests/test_helpdesk_team_privacy_visibility.py
@@ -1,35 +1,7 @@
-from odoo.addons.helpdesk.tests.test_helpdesk_team_privacy_visibility import (
-    TestHelpdeskTeamPrivacyVisibility,
-)
-from odoo.addons.mail.tests.common import mail_new_test_user
+from .common import TestHelpdeskPrivacyCommon
 
 
-class TestHelpdeskTeamPrivacyVisibility(TestHelpdeskTeamPrivacyVisibility):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        cls.portal_user_a = mail_new_test_user(
-            cls.env,
-            name="helpdesk_portal_a",
-            login="helpdesk_portal_a",
-            email="helpdesk_a@portal.com",
-            notification_type="email",
-            groups="base.group_portal",
-        )
-
-        cls.portal_user_b = mail_new_test_user(
-            cls.env,
-            name="helpdesk_portal_b",
-            login="helpdesk_portal_b",
-            email="helpdesk_b@portal.com",
-            notification_type="email",
-            groups="base.group_portal",
-        )
-
-        cls.portal_user_a.partner_id.commercial_partner_id = cls.partner.id
-        cls.portal_user_b.partner_id.commercial_partner_id = cls.partner.id
-
+class TestHelpdeskTeamPrivacyVisibility(TestHelpdeskPrivacyCommon):
     def test_helpdesk_team_visibility_private(self):
         self.ticket.write({"partner_id": self.partner.id})
 
