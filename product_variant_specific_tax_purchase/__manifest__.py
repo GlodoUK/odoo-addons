08cd6c25d9f021207d4ce6883342b89c417c93da
GarethGloSystems <gareth@glo.systems>
2025-06-09 09:06:31 +0100

    product_variant_specific_tax_purchase_18.0

---
+++ product_variant_specific_tax_purchase/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "product_variant_specific_tax_purchase",
+    "version": "18.0.1.0.0",
+    "author": "Glo Networks",
+    "website": "https://www.glo.systems/",
+    "depends": ["purchase"],
+    "data": ["views/product_product_views.xml"],
+    "license": "Other proprietary",
+}
---
+++ product_variant_specific_tax_purchase/models/product_product.py
@@ -0,0 +1,34 @@
+from odoo import api, fields, models
+
+
+class Product(models.Model):
+    _inherit = "product.product"
+
+    supplier_taxes_id = fields.Many2many(
+        "account.tax",
+        "product_product_supplier_taxes_rel",
+        "prod_id",
+        "tax_id",
+        compute="_compute_supplier_taxes_id",
+        inverse="_inverse_supplier_taxes_id",
+        store=True,
+        string="Purchase Taxes",
+    )
+
+    variant_supplier_taxes_id = fields.Many2many(
+        "account.tax",
+        "product_product_variant_supplier_taxes_rel",
+        "prod_id",
+        "tax_id",
+        domain=[("type_tax_use", "=", "purchase")],
+        string="Variant Purchase Taxes",
+    )
+
+    @api.depends("product_tmpl_id.supplier_taxes_id", "variant_supplier_taxes_id")
+    def _compute_supplier_taxes_id(self):
+        for product in self:
+            product.supplier_taxes_id = product.variant_supplier_taxes_id or product.product_tmpl_id.supplier_taxes_id
+
+    def _inverse_supplier_taxes_id(self):
+        for product in self:
+            product.product_tmpl_id.supplier_taxes_id = product.supplier_taxes_id
