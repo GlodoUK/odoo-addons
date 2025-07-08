3eba5d11614f7c6ea47e04e7c3c8bab3420dbb2d
ellbristow <elliott@glo.systems>
2025-07-08 10:52:56 +0200

    Generalise postmark module

---
+++ mail_postmark_email_header/README.rst
@@ -0,0 +1,5 @@
+Mail Postmark Email Header
+==========================
+
+This module adds a header to all outgoing emails to prevent Postmark stripping
+Odoo-specific headers required for handling replies.
---
+++ mail_postmark_email_header/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Mail Postmark Email Header",
+    "version": "18.0.1.0.0",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "category": "Mail",
+    "depends": ["mail"],
+    "license": "Other proprietary",
+}
