cc5eabd8a9301f1c5030bde7abcd48b408223f0e
Elliott Bristow <admin@ellitopia.com>
2025-07-08 11:10:09 +0200

    Generalise postmark module (#134)

---
+++ mail_postmark_email_header/models/ir_mail_server.py
@@ -0,0 +1,50 @@
+from odoo import models
+
+
+class IrMailServer(models.Model):
+    _inherit = "ir.mail_server"
+
+    def build_email(
+        self,
+        email_from,
+        email_to,
+        subject,
+        body,
+        email_cc=None,
+        email_bcc=None,
+        reply_to=False,
+        attachments=None,
+        message_id=None,
+        references=None,
+        object_id=False,
+        subtype="plain",
+        headers=None,
+        body_alternative=None,
+        subtype_alternative="plain",
+    ):
+        if not headers:
+            headers = {}
+
+        headers.update(
+            {
+                "X-PM-KeepID": "true",
+            }
+        )
+
+        return super().build_email(
+            email_from,
+            email_to,
+            subject,
+            body,
+            email_cc=email_cc,
+            email_bcc=email_bcc,
+            reply_to=reply_to,
+            attachments=attachments,
+            message_id=message_id,
+            references=references,
+            object_id=object_id,
+            subtype=subtype,
+            headers=headers,
+            body_alternative=body_alternative,
+            subtype_alternative=subtype_alternative,
+        )
