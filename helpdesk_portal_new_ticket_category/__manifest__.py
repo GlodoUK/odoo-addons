de070d58a6a387c9b8536675ccf00a57d4a58d29
GarethGloSystems <gareth@glo.systems>
2025-07-09 14:27:37 +0100

    Bootstrap helpdesk_portal_new_ticket_category 18.0

---
+++ helpdesk_portal_new_ticket_category/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "helpdesk_portal_new_ticket_category",
+    "version": "18.0.1.0.0",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "depends": [
+        "helpdesk_portal_new_ticket",
+        "helpdesk_ticket_category",
+    ],
+    "data": [
+        "views/helpdesk_portal_templates.xml"
+    ],
+    "license": "Other proprietary",
+}
