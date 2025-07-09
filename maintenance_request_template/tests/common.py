a339f3a81b572e360d91c10a8f3d2e433187ec74
GarethGloSystems <gareth@glo.systems>
2025-07-09 09:35:50 +0100

    pre-commit

--- maintenance_request_template/tests/common.py
+++ maintenance_request_template/tests/common.py
@@ -12,28 +12,38 @@ class MaintenanceRequestTemplateCommon(TransactionCase):
         cls.EquipmentTemplate = cls.env["maintenance.equipment.template"]
         cls.Request = cls.env["maintenance.request"]
 
-        cls.templateA = cls.EquipmentTemplate.create({
-            "name": "Template A",
-            "description": "Equipment Description",
-        })
+        cls.templateA = cls.EquipmentTemplate.create(
+            {
+                "name": "Template A",
+                "description": "Equipment Description",
+            }
+        )
 
-        cls.templateB = cls.EquipmentTemplate.create({
-            "name": "Template B",
-            "description": "Categ Description",
-        })
+        cls.templateB = cls.EquipmentTemplate.create(
+            {
+                "name": "Template B",
+                "description": "Categ Description",
+            }
+        )
 
-        cls.categA = cls.EquipmentCateg.create({
-            "name": "CategA",
-            "template_id": cls.templateB.id,
-        })
+        cls.categA = cls.EquipmentCateg.create(
+            {
+                "name": "CategA",
+                "template_id": cls.templateB.id,
+            }
+        )
 
-        cls.equipmentA = cls.Equipment.create({
-            "name": "EquipmentA",
-            "category_id": cls.categA.id,
-            "template_id": cls.templateA.id,
-        })
+        cls.equipmentA = cls.Equipment.create(
+            {
+                "name": "EquipmentA",
+                "category_id": cls.categA.id,
+                "template_id": cls.templateA.id,
+            }
+        )
 
-        cls.equipmentB = cls.Equipment.create({
-            "name": "EquipmentB",
-            "category_id": cls.categA.id,
-        })
+        cls.equipmentB = cls.Equipment.create(
+            {
+                "name": "EquipmentB",
+                "category_id": cls.categA.id,
+            }
+        )
--- maintenance_request_template/tests/test_maintenance_request_template.py
+++ maintenance_request_template/tests/test_maintenance_request_template.py
@@ -5,10 +5,12 @@ from .common import MaintenanceRequestTemplateCommon
 
 class TestMaintenanceRequestTemplate(MaintenanceRequestTemplateCommon):
     def test_template_onchange(self):
-        request_id = self.Request.create({
-            "name": "Request",
-            "stage_id": self.env.ref("maintenance.stage_0").id,
-        })
+        request_id = self.Request.create(
+            {
+                "name": "Request",
+                "stage_id": self.env.ref("maintenance.stage_0").id,
+            }
+        )
 
         with Form(request_id) as request_form:
             request_form.equipment_id = self.equipmentA
@@ -28,11 +30,13 @@ class TestMaintenanceRequestTemplate(MaintenanceRequestTemplateCommon):
             )
 
     def test_template_category(self):
-        request_id = self.Request.create({
-            "name": "Request",
-            "equipment_id": self.equipmentB.id,
-            "stage_id": self.env.ref("maintenance.stage_0").id,
-        })
+        request_id = self.Request.create(
+            {
+                "name": "Request",
+                "equipment_id": self.equipmentB.id,
+                "stage_id": self.env.ref("maintenance.stage_0").id,
+            }
+        )
 
         self.assertEqual(
             request_id.description,
@@ -40,11 +44,13 @@ class TestMaintenanceRequestTemplate(MaintenanceRequestTemplateCommon):
         )
 
     def test_template_equipment(self):
-        request_id = self.Request.create({
-            "name": "Request",
-            "equipment_id": self.equipmentA.id,
-            "stage_id": self.env.ref("maintenance.stage_0").id,
-        })
+        request_id = self.Request.create(
+            {
+                "name": "Request",
+                "equipment_id": self.equipmentA.id,
+                "stage_id": self.env.ref("maintenance.stage_0").id,
+            }
+        )
 
         self.assertEqual(
             request_id.description,
@@ -56,11 +62,13 @@ class TestMaintenanceRequestTemplate(MaintenanceRequestTemplateCommon):
         self.equipmentA.category_id.template_id.description = False
         self.equipmentA.note = "equipmentA Note"
 
-        request_id = self.Request.create({
-            "name": "Request",
-            "equipment_id": self.equipmentA.id,
-            "stage_id": self.env.ref("maintenance.stage_0").id,
-        })
+        request_id = self.Request.create(
+            {
+                "name": "Request",
+                "equipment_id": self.equipmentA.id,
+                "stage_id": self.env.ref("maintenance.stage_0").id,
+            }
+        )
 
         self.assertEqual(
             request_id.description,
