e46d9990c25cc3fed9c51a932690f22943dcd2f9
GarethGloSystems <gareth@glo.systems>
2025-07-30 12:38:28 +0100

    [FEAT] whistl_get_tracking_link method

--- delivery_parcelhub_whistl/models/delivery_carrier.py
+++ delivery_parcelhub_whistl/models/delivery_carrier.py
@@ -162,6 +162,10 @@ class DeliveryCarrier(models.Model):
         default="6",
     )  # TODO: Flesh this out, or pull from API
 
+    def whistl_get_tracking_link(self, picking):
+        formatted_zip = picking.partner_id.zip.replace(" ", "").upper()
+        return f"https://web.parcelhub.net/Tracking/reference/{picking.carrier_tracking_ref}/postcode/{formatted_zip}"
+
     def _whistl_ensure_valid_shipping(self, pickings):
         MAX_LEN = 32
 
