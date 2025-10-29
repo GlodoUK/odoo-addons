4ebed4777138ab23ddc9ddb54da73de477c50b48
GarethGloSystems <gareth@glo.systems>
2025-10-29 08:18:04 +0000

    [MIG] web_cmd_search: Migration to 19.0

--- web_cmd_search/__manifest__.py
+++ web_cmd_search/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Glo Networks",
     "website": "https://github.com/GlodoUK/odoo-addons",
     "category": "Uncategorized",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "depends": ["base", "web"],
     "data": [
         "security/ir.model.access.csv",
