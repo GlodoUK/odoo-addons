3e51709c817f30d7ae936eb7eeb55cfdcb717c54
GarethGloSystems <gareth@glo.systems>
2025-10-29 08:27:28 +0000

    [MIG] helpdesk_sale_order_account_move_link: Migration to 19.0

--- helpdesk_sale_order_account_move_link/__manifest__.py
+++ helpdesk_sale_order_account_move_link/__manifest__.py
@@ -1,11 +1,8 @@
 {
     "name": "Helpdesk Sale Order Account Move Link",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Glo Networks",
     "website": "https://github.com/GlodoUK/odoo-addons",
-    "depends": [
-        "helpdesk_account_move_link",
-        "helpdesk_sale_order_link",
-    ],
+    "depends": ["helpdesk_account_move_link", "helpdesk_sale_order_link"],
     "license": "Other proprietary",
 }
