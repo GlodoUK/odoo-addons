6bfa3f289373b2cf128ba43b9edd50c54f34e9cf
GarethGloSystems <gareth@glo.systems>
2026-03-06 13:00:34 +0000

    [FEAT] web_graph_bar_zerofill module

---
+++ web_graph_bar_zerofill/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Web Graph Bar Zero Fill",
+    "version": "18.0.1.0.0",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "web_graph_bar_zerofill/static/src/js/graph_model_patch.esm.js",
+        ],
+    },
+    "license": "LGPL-3",
+}
