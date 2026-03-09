3989c5a2c782295f3c1be43d12f082cd35bbe439
Elliott Bristow <elliott@glo.systems>
2026-03-09 10:02:41 +0100

    [15.0] Backport glodo_client

---
+++ glodo_client/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Glodo Cloud Client",
+    "summary": "Server-wide client for Glodo Cloud remote instance management",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "category": "Technical",
+    "version": "15.0.1.0.0",
+    "depends": ["base"],
+    "external_dependencies": {
+        "python": ["cryptography"],
+    },
+    "license": "Other proprietary",
+}
