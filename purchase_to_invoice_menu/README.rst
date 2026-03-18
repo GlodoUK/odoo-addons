bef3656f8260d98b3199142acf629c40e2315616
GarethGloSystems <gareth@glo.systems>
2026-03-18 10:19:23 +0000

    [MIG] purchase_invoice_menu: Migration to 19.0

--- purchase_to_invoice_menu/README.rst
+++ purchase_to_invoice_menu/README.rst
@@ -2,6 +2,6 @@
 purchase_to_invoice_menu
 ------------------------
 
-Adds a `To Invoice` menu to Purchases, much like the Sales menu.
+Adds a ``Orders To Invoice`` menu to Purchase, much like the Sales menu.
 
-It is highly recommended to pair this with the OCA module `purchase_open_qty`
+It is highly recommended to pair this with the OCA module ``purchase_open_qty``
--- purchase_to_invoice_menu/__manifest__.py
+++ purchase_to_invoice_menu/__manifest__.py
@@ -1,14 +1,10 @@
 {
     "name": "purchase_to_invoice_menu",
-    "summary": "Adds a 'To Invoice' menu to the Purchase menu",
+    "summary": "Adds a 'Orders To Invoice' menu to the Purchase menu",
+    "version": "19.0.1.0.0",
     "author": "Glo Networks",
-    "website": "https://github.com/GlodoUK/purchase",
-    "category": "Uncategorized",
-    "version": "17.0.0.0.0",
+    "website": "https://github.com/GlodoUK/odoo-addons",
     "depends": ["purchase"],
-    "data": [
-        "views/menu.xml",
-    ],
-    "demo": [],
+    "data": ["views/purchase_views.xml"],
     "license": "AGPL-3",
 }
