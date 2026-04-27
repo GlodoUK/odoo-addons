8e4697df46a00ac778ecdf6156488e7ef4b08ace
GarethGloSystems <gareth@glo.systems>
2026-04-27 13:28:38 +0100

    [18.0][FEAT] cloc Implementation (client-side backport of #247)

--- glodo_client/__manifest__.py
+++ glodo_client/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "18.0.1.0.0",
     "depends": ["base"],
     "external_dependencies": {
-        "python": ["cryptography"],
+        "python": ["cryptography", "manifestoo_core"],
     },
     "license": "Other proprietary",
 }
