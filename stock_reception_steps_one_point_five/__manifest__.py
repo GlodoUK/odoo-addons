72faef1f9ed393391b0471e76f14e00a8d4f8214
Karl Southern <karl@theangryangel.co.uk>
2026-06-16 10:16:23 +0100

    [ADD] stock_reception_steps_one_point_five
    Fixes T16811

---
+++ stock_reception_steps_one_point_five/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "stock_reception_steps_one_point_five",
+    "summary": "Receive into a landing area (Input) and store to Stock manually",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "category": "Inventory/Inventory",
+    "version": "19.0.1.0.0",
+    "depends": ["stock"],
+    "data": [],
+    "license": "LGPL-3",
+}
