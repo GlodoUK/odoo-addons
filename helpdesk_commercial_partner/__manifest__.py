2142f02e24d3b5a19a1f1ef472cea87ad1235845
FintanGlo <fintan@glo.systems>
2026-06-22 14:25:31 +0100

    GH209689 fix commercial_partner_id not appearing on helpdesk ticket report

--- helpdesk_commercial_partner/__manifest__.py
+++ helpdesk_commercial_partner/__manifest__.py
@@ -4,6 +4,6 @@
     "author": "Glo Networks",
     "website": "https://github.com/GlodoUK/odoo-addons",
     "depends": ["helpdesk"],
-    "data": ["views/helpdesk_ticket_views.xml"],
+    "data": ["views/helpdesk_ticket_views.xml", "report/helpdesk_ticket_analysis.xml"],
     "license": "Other proprietary",
 }
