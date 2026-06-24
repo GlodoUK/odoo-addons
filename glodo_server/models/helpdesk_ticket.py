94417a8590881d85e3975b70f5d9cc94297c9d20
FintanGlo <fintan@glo.systems>
2026-06-24 09:34:55 +0100

    commercial_partner_id fix

--- glodo_server/models/helpdesk_ticket.py
+++ glodo_server/models/helpdesk_ticket.py
@@ -4,7 +4,9 @@ from odoo import fields, models
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
-    glodo_instance_ids = fields.One2many(related="partner_id.glodo_instance_ids")
+    glodo_instance_ids = fields.One2many(
+        related="partner_id.commercial_partner_id.glodo_instance_ids"
+    )
 
     def action_view_instance(self):
         """View instance for this contact."""
@@ -15,7 +17,7 @@ class HelpdeskTicket(models.Model):
             "name": self.env._("Instance - %(name)s", name=self.name),
             "res_model": "glodo.instance",
             "view_mode": "form",
-            "domain": [("partner_id", "=", self.id)],
+            "domain": [("partner_id.commercial_partner_id", "=", self.id)],
             "res_id": self.glodo_instance_ids[0].id
             if len(self.glodo_instance_ids) == 1
             else False,
