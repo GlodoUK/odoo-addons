95b8d8b3773397fc44e199a86a5c12ef893fdb83
Karl Southern <karl@theangryangel.co.uk>
2026-06-30 15:59:22 +0100

    [ADD] account_tax_plastic: Add a basic plastic tastic implementation, Fixes T17046

---
+++ account_tax_plastic/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Plastic Tax",
+    "summary": "Plastic tax calculated from product plastic weight",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "category": "Accounting/Accounting",
+    "version": "19.0.1.0.0",
+    "depends": ["account_tax_python", "product"],
+    "data": [
+        "data/account_tax_group.xml",
+        "data/account_tax.xml",
+        "views/product_template_views.xml",
+    ],
+    "license": "LGPL-3",
+}
