dfe4a392188bf4a7f6cdb67cf168b729d8c261b0
Elliott Bristow <admin@ellitopia.com>
2026-07-03 16:45:19 +0200

    [18.0][ADD] GH209548 Automated Export From Template module (#289)
    * [ADD] GH209548 Automated Export From Template module
    
    * [FIX] Pre-commit issues
    
    * Update test_ensure_valid.py
    
    * Add empty line for code readability in test file
    
    ---------
    
    Co-authored-by: Karl <karl@theangryangel.co.uk>

---
+++ automated_export_from_template/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Automated Export From Template",
+    "version": "18.0.1.0.0",
+    "summary": "Scheduled export of records using a saved export template",
+    "category": "Tools",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/odoo-addons",
+    "depends": ["mail"],
+    "data": [],
+    "demo": [],
+    "license": "Other proprietary",
+}
