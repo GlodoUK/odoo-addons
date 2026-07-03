67853f9c1663e8190843fd2912ad40f71f50c619
Elliott Bristow <elliott@glo.systems>
2026-07-03 13:10:20 +0200

    [ADD] GH209548 Automated Export From Template module

---
+++ automated_export_from_template/README.rst
@@ -0,0 +1,10 @@
+Automated Export
+================
+
+Scheduled export of a model's records to XLSX/CSV using a saved
+("Favorites > Save current export") export template, emailed as an
+attachment.
+
+The included example scheduled action is disabled by default - set the
+export template name, recipient email address and domain to match
+your needs, then activate it.
---
+++ automated_export_from_template/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Automated Export From Template",
+    "version": "18.0.1.0.0",
+    "summary": "Scheduled export of records using a saved export template",
+    "category": "Tools",
+    "author": "Glo Networks",
+    "website": "http://glo.systems/",
+    "depends": ["mail"],
+    "data": [
+        "data/ir_cron.xml",
+    ],
+    "license": "Other proprietary",
+}
