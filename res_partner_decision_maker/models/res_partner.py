1018f32906a23ea2ffc842ff769aedf1766e07b3
Karl Southern <karl@theangryangel.co.uk>
2026-07-29 10:07:27 +0100

    [FIX] res_partner_decision_maker: Original implementation was not as strict on CI

--- res_partner_decision_maker/models/res_partner.py
+++ res_partner_decision_maker/models/res_partner.py
@@ -5,6 +5,12 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     decision_maker_id = fields.Many2one("res.partner")
-    decision_maker_email = fields.Char(related="decision_maker_id.email")
-    decision_maker_function = fields.Char(related="decision_maker_id.function")
-    decision_maker_phone = fields.Char(related="decision_maker_id.phone")
+    decision_maker_email = fields.Char(
+        related="decision_maker_id.email", string="Decision Maker Email"
+    )
+    decision_maker_function = fields.Char(
+        related="decision_maker_id.function", string="Decision Maker Job Position"
+    )
+    decision_maker_phone = fields.Char(
+        related="decision_maker_id.phone", string="Decision Maker Phone"
+    )
